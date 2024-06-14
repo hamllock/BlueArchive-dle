82ced619d316ba9981fff2935210659baece75f4
hamllock <h4mlock@gmail.com>
2024-06-14 23:08:54 +0800

    added variance

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,12 +1,32 @@
-const Cell = ({ content, image }: { content?: string; image?: any }) => {
+type CellProps = {
+  content?: string;
+  image?: any;
+  variant?: "red" | "green";
+};
+
+const Cell = ({ content, image, variant }: CellProps) => {
+  let bgColor = "";
+  switch (variant) {
+    case "red":
+      bgColor = "bg-red-500";
+      break;
+    case "green":
+      bgColor = "bg-green-500";
+      break;
+    default:
+      bgColor = "";
+  }
+
   return (
     <>
-      <div className="flex w-28 h-28 justify-center items-center grow-0 border-solid border-2">
+      <div
+        className={`flex w-28 h-28 justify-center items-center grow-0 border-solid border ${bgColor}`}
+      >
         {image && <img className="object-contain h-full w-full" src={image} />}
-
         {content}
       </div>
     </>
   );
 };
+
 export default Cell;
