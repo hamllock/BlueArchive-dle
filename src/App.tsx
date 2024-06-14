521eec87158ed692785778087637398cf77f07d1
hamllock <h4mlock@gmail.com>
2024-06-14 23:07:45 +0800

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import AnswerBar from "./components/AnswerBar.tsx";
 import Table from "./components/Table.tsx";
+import useStudent from "./hooks/useStudent";
+import students from "./data/students.json";
 
 function App() {
+  const { randStudent, resetList, studentList, addStudent } =
+    useStudent(students);
   return (
-    <div>
-      <Table />
+    <div className="flex flex-col gap-4 items-center h-screen w-screen">
+      <AnswerBar addStudent={addStudent} resetList={resetList} />
+      <Table studentList={studentList} randStudent={randStudent} />
     </div>
   );
 }
