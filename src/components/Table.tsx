e150aaf851b8d9128480ad535437b8ac46683df1
hamllock <h4mlock@gmail.com>
2024-06-14 23:08:42 +0800

    improved Table component rendering and added variance conditionals

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,52 +1,62 @@
-// import useStudent from "../hooks/useStudent";
-import students from "../data/students.json";
-import locals from "../data/localization.json";
 import Cell from "./Cell";
-import { Students, Localization } from "../types";
+import local from "../data/localization.json";
+import { Students, Student, Localization } from "../types";
 
-import { useState } from "react";
+const Table: React.FC<{ studentList: Students; randStudent: Student }> = ({
+  studentList,
+  randStudent,
+}) => {
+  const localization = local as Localization; // Add this line
 
-const useStudent = (students: Students, locals: Localization) => {
-  const [count, setCount] = useState(
-    Math.floor(Math.random() * students.length)
-  );
-  const [studentList, setStudentList] = useState<Students>([]);
-  const [local] = useState(locals);
-
-  const addStudent = () => {
-    setStudentList((prevList) => [...prevList, students[count]]);
-    setCount(Math.floor(Math.random() * students.length));
-  };
-
-  return {
-    studentList,
-    local,
-    addStudent,
-  };
-};
-
-const Table = () => {
-  const { studentList, local, addStudent } = useStudent(students, locals);
   return (
     <>
-      <div className="flex flex-col gap-4 items-center h-screen w-screen">
-        <div className="flex flex-row gap-2">
-          <button onClick={addStudent}>Add student</button>
-        </div>
-        <div>
-          {studentList.map((student, index) => (
-            <div key={index} className="flex flex-row">
-              <Cell image={`./icon/${student.Id}.webp`} />
-              <Cell content={`${student.FamilyName} ${student.Name}`} />
-              <Cell content={`${local.SchoolLong[student.School]}`} />
-              <Cell content={`${local.Club[student.Club]}`} />
-              <Cell content={`${local.SquadType[student.SquadType]}`} />
-              <Cell content={`${local.TacticRole[student.TacticRole]}`} />
-              <Cell content={`${local.BulletType[student.BulletType]}`} />
-              <Cell content={`${local.ArmorTypeLong[student.ArmorType]}`} />
-            </div>
-          ))}
-        </div>
+      <div className="flex flex-col gap-4">
+        {studentList.map((student, index) => (
+          <div key={index} className="flex flex-row gap-1">
+            <Cell image={`./icon/${student.Id}.webp`} />
+            <Cell
+              content={`${student.FamilyName} ${student.Name}`}
+              variant={
+                randStudent.Name === student.Name &&
+                randStudent.FamilyName === student.FamilyName
+                  ? "green"
+                  : "red"
+              }
+            />
+            <Cell
+              content={`${localization.SchoolLong[student.School]}`}
+              variant={randStudent.School === student.School ? "green" : "red"}
+            />
+            <Cell
+              content={`${localization.Club[student.Club]}`}
+              variant={randStudent.Club === student.Club ? "green" : "red"}
+            />
+            <Cell
+              content={`${localization.SquadType[student.SquadType]}`}
+              variant={
+                randStudent.SquadType === student.SquadType ? "green" : "red"
+              }
+            />
+            <Cell
+              content={`${localization.TacticRole[student.TacticRole]}`}
+              variant={
+                randStudent.TacticRole === student.TacticRole ? "green" : "red"
+              }
+            />
+            <Cell
+              content={`${localization.BulletType[student.BulletType]}`}
+              variant={
+                randStudent.BulletType === student.BulletType ? "green" : "red"
+              }
+            />
+            <Cell
+              content={`${localization.ArmorTypeLong[student.ArmorType]}`}
+              variant={
+                randStudent.ArmorType === student.ArmorType ? "green" : "red"
+              }
+            />
+          </div>
+        ))}
       </div>
     </>
   );
