a1f43e7b453c9100b80e873691fb980602126bec
hamllock <h4mlock@gmail.com>
2024-06-15 14:51:47 +0800

    chore: Update ESLint and Prettier configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,15 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+    'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+  }
+};
