ce3ee41712b657174230e52b6c9de776bf91621f
hamllock <h4mlock@gmail.com>
2024-07-31 00:29:46 +0800

    feat: Update Cell component to use 'state' instead of 'variant'

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,27 +1,27 @@
 type CellProps = {
   content?: string;
   image?: any;
-  variant?: "red" | "green";
+  state?: 'red' | 'green';
+  textColor?: string;
 };
 
-const Cell = ({ content, image, variant }: CellProps) => {
-  let bgColor = "";
-  switch (variant) {
-    case "red":
-      bgColor = "bg-red-500";
+const Cell = ({ content, image, state, textColor }: CellProps) => {
+  let bgColor = '';
+  switch (state) {
+    case 'red':
+      bgColor = 'bg-red-500';
       break;
-    case "green":
-      bgColor = "bg-green-500";
+    case 'green':
+      bgColor = 'bg-green-500';
       break;
     default:
-      bgColor = "";
+      bgColor = '';
   }
 
   return (
     <>
       <div
-        className={`flex w-28 h-28 justify-center items-center grow-0 border-solid border ${bgColor}`}
-      >
+        className={`flex w-28 h-28 justify-center items-center grow-0 border-solid border ${bgColor} ${textColor}`}>
         {image && <img className="object-contain h-full w-full" src={image} />}
         {content}
       </div>
