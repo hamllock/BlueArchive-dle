0b47ae43278165c27e76d13b39c88c843201e394
hamllock <h4mlock@gmail.com>
2024-07-31 00:29:14 +0800

    chore: Add Blue Archive color palette to Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        BlueArchiveRED: '#880d1d',
+        BlueArchiveYELLOW: '#ac882b',
+        BlueArchiveBLUE: '#337296',
+        BlueArchivePURPLE: '#8455a1',
+        BlueArchiveNORMAL: '#484e6e'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
